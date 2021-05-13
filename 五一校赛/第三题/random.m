4f82bfad9b325205a6503e9d0ce243d842236bd3
HHU-ZYP <82154541+HHU-ZYP@users.noreply.github.com>
2021-05-13 16:22:22 +0800

    Add files via upload

---
+++ 五一校赛/第三题/random.m
@@ -0,0 +1,50 @@
+clc;clear
+fprintf("0 %d",normrnd(13.284036,3.64472715,1,1));
+fprintf(" 1 %d",normrnd(14.962134,3.868091778,1,1));
+fprintf(" 2 %d",normrnd(19.846022,4.454887428,1,1));
+fprintf(" 3 %d\n\t",normrnd(20.012922,4.473580445,1,1));
+
+fprintf("0 %d",normrnd(9.87088,3.141795665,1,1));
+fprintf(" 1 %d",normrnd(19.907534,4.461785965,1,1));
+fprintf(" 2 %d",normrnd(17.928158,4.234165561,1,1));
+fprintf(" 3 %d\n\t",normrnd(18.942358,4.35228193,1,1));
+
+fprintf("0 %d",normrnd(20.058414,4.478662077,1,1));
+fprintf(" 1 %d",normrnd(15.97263,3.996577286,1,1));
+fprintf(" 2 %d",normrnd(14.970352,3.869153913,1,1));
+fprintf(" 3 %d\n\t",normrnd(15.116388,3.887979938,1,1));
+
+fprintf("0 %d",normrnd(7.988652,2.826420351,1,1));
+fprintf(" 1 %d",normrnd(9.936646,3.152244597,1,1));
+fprintf(" 2 %d",normrnd(5.935868,2.436363684,1,1));
+fprintf(" 3 %d\n\t",normrnd(18.128398,4.257745648,1,1));
+
+fprintf("0 %d",normrnd(8.770062,2.961429047,1,1));
+fprintf(" 1 %d",normrnd(13.722012,3.70432342,1,1));
+fprintf(" 2 %d",normrnd(13.005208,3.606273423,1,1));
+fprintf(" 3 %d\n\t",normrnd(11.24949,3.354025939,1,1));
+
+fprintf("0 %d",normrnd(19.07412,4.367392815,1,1));
+fprintf(" 1 %d",normrnd(20.094358,4.482673086,1,1));
+fprintf(" 2 %d",normrnd(14.148532,3.761453442,1,1));
+fprintf(" 3 %d\n\t",normrnd(13.883878,3.726107621,1,1));
+
+fprintf("0 %d",normrnd(11.160148,3.340680769,1,1));
+fprintf(" 1 %d",normrnd(16.496146,4.06154478,1,1));
+fprintf(" 2 %d",normrnd(12.013664,3.466073283,1,1));
+fprintf(" 3 %d\n\t",normrnd(15.550634,3.943429218,1,1));
+
+fprintf("0 %d",normrnd(16.0201,4.002511711,1,1));
+fprintf(" 1 %d",normrnd(8.82748,2.971107538,1,1));
+fprintf(" 2 %d",normrnd(18.114384,4.256099623,1,1));
+fprintf(" 3 %d\n\t",normrnd(16.831394,4.102608195,1,1));
+
+fprintf("0 %d",normrnd(15.014602,3.874867998,1,1));
+fprintf(" 1 %d",normrnd(12.03512,3.469167047,1,1));
+fprintf(" 2 %d",normrnd(7.041914,2.653660491,1,1));
+fprintf(" 3 %d\n\t",normrnd(8.949654,2.991597232,1,1));
+
+fprintf("0 %d",normrnd(12.952448,3.598950958,1,1));
+fprintf(" 1 %d",normrnd(7.010982,2.647825901,1,1));
+fprintf(" 2 %d",normrnd(9.04917,3.008183837,1,1));
+fprintf(" 3 %d\n\t",normrnd(16.052406,4.006545395,1,1));
---
+++ 五一校赛/第二题/random.m
@@ -0,0 +1,50 @@
+clc;clear
+fprintf("0 %d",normrnd(13.284036,1.262528917,1,1));
+fprintf(" 1 %d",normrnd(14.962134,1.040711667,1,1));
+fprintf(" 2 %d",normrnd(19.846022,1.089726857,1,1));
+fprintf(" 3 %d\n\t",normrnd(20.012922,1.374675342,1,1));
+
+fprintf("0 %d",normrnd(9.87088,0.912844245,1,1));
+fprintf(" 1 %d",normrnd(19.907534,1.119335195,1,1));
+fprintf(" 2 %d",normrnd(17.928158,0.949834206,1,1));
+fprintf(" 3 %d\n\t",normrnd(18.942358,0.943654882,1,1));
+
+fprintf("0 %d",normrnd(20.058414,0.919125009,1,1));
+fprintf(" 1 %d",normrnd(15.97263,0.832702936,1,1));
+fprintf(" 2 %d",normrnd(14.970352,1.019822179,1,1));
+fprintf(" 3 %d\n\t",normrnd(15.116388,0.90120593,1,1));
+
+fprintf("0 %d",normrnd(7.988652,0.965490089,1,1));
+fprintf(" 1 %d",normrnd(9.936646,0.976417415,1,1));
+fprintf(" 2 %d",normrnd(5.935868,0.19492674,1,1));
+fprintf(" 3 %d\n\t",normrnd(18.128398,1.061939536,1,1));
+
+fprintf("0 %d",normrnd(8.770062,0.863185422,1,1));
+fprintf(" 1 %d",normrnd(13.722012,1.067506779,1,1));
+fprintf(" 2 %d",normrnd(13.005208,0.911757145,1,1));
+fprintf(" 3 %d\n\t",normrnd(11.24949,1.109426758,1,1));
+
+fprintf("0 %d",normrnd(19.07412,1.155415745,1,1));
+fprintf(" 1 %d",normrnd(20.094358,0.986646508,1,1));
+fprintf(" 2 %d",normrnd(14.148532,0.935659811,1,1));
+fprintf(" 3 %d\n\t",normrnd(13.883878,1.073187377,1,1));
+
+fprintf("0 %d",normrnd(11.160148,1.013068014,1,1));
+fprintf(" 1 %d",normrnd(16.496146,0.964085394,1,1));
+fprintf(" 2 %d",normrnd(12.013664,0.886688065,1,1));
+fprintf(" 3 %d\n\t",normrnd(15.550634,0.810161883,1,1));
+
+fprintf("0 %d",normrnd(16.0201,1.063825743,1,1));
+fprintf(" 1 %d",normrnd(8.827480,0.511280282,1,1));
+fprintf(" 2 %d",normrnd(18.114384,1.034413526,1,1));
+fprintf(" 3 %d\n\t",normrnd(16.831394,0.95538933,1,1));
+
+fprintf("0 %d",normrnd(15.014602,0.986816797,1,1));
+fprintf(" 1 %d",normrnd(12.035121,0.76138787,1,1));
+fprintf(" 2 %d",normrnd(7.041914,0.365599114,1,1));
+fprintf(" 3 %d\n\t",normrnd(8.949654,0.418563054,1,1));
+
+fprintf("0 %d",normrnd(12.952448,0.455512309,1,1));
+fprintf(" 1 %d",normrnd(7.010982,0.531209821,1,1));
+fprintf(" 2 %d",normrnd(9.04917,0.455372472,1,1));
+fprintf(" 3 %d\n\t",normrnd(16.052406,0.513219576,1,1));
